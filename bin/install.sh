23a86a0992a47808a16c21f48eb877a02aa8b2f4
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2013-10-17 21:43:34 +0900

    change

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,3 @@ python virturlenv.py mypython
 source mypython/bin/activate
 pip install pillow
 pip install https://github.com/mapproxy/mapproxy/tarball/master
-cp ../project/cjp_src/tile.py mypython/lib/python2.7/site-packages/mapproxy/client/
-cp ../project/cjp_src/message.py mypython/lib/python2.7/site-packages/mapproxy/image/
-cp ../project/cjp_src/ipaexg.ttf mypython/lib/python2.7/site-packages/mapproxy/image/fonts/
