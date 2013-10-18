19ae729921af6ef5f5365cbda4a7482c0eb15809
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2013-10-18 14:09:05 +0900

    added for custom_jp

---
+++ i18n/mapproxyplugin_ja.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>message</name>
+    <message>
+        <location filename="mapproxy_plugin.py" line="101"/>
+        <source>You need to install mapproxy first</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="107"/>
+        <source>Are you sure you want to run mapproxy? It will run on Command Prompt.Please close it by hand when you&apos;ll stop.You can confirm the state on http://localhost:8080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="109"/>
+        <source>Are you sure you want to run mapproxy? You can confirm the state on http://localhost:8080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="117"/>
+        <source>This plugin limits the ability to print. You need to accept these Terms of Use(http://portal.cyberjapan.jp/portalsite/kiyaku/ , http://map.ecoris.info)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="148"/>
+        <source>Are you sure you want to install mapproxy?So it will take a time to install, please wait for the message to appear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="153"/>
+        <source>mapproxy install finished</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="155"/>
+        <source>mapproxy install fail !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="158"/>
+        <source>Are you sure you want to remove cache?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
