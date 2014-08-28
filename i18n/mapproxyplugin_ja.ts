c720d43a30b564b50177fe44f57c7ac1a8cd23c0
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2014-08-28 18:51:22 +0900

    add ja.ts

---
+++ i18n/mapproxyplugin_ja.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>message</name>
+    <message>
+        <location filename="mapproxy_plugin.py" line="100"/>
+        <source>You need to install mapproxy first</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="107"/>
+        <source>Are you sure you want to run mapproxy? It will run on Command Prompt.Please close it by hand when you&apos;ll stop.You can confirm the state on http://localhost:8080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="111"/>
+        <source>Are you sure you want to run mapproxy? You can confirm the state on http://localhost:8080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="138"/>
+        <source>Are you sure you want to install mapproxy?So it will take a time to install, please wait for the message to appear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="145"/>
+        <source>mapproxy install finished</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="148"/>
+        <source>mapproxy install fail !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mapproxy_plugin.py" line="152"/>
+        <source>Are you sure you want to remove cache?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
