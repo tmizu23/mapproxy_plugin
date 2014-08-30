64dafe0f61c357d069ec403de5c64c55736ee1b6
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2014-08-30 17:50:15 +0900

    change

--- bin/layers.sh
+++ bin/layers.sh
@@ -1,4 +1,4 @@
-﻿script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 cd $script_dir
 source mypython/bin/activate
 python layers.py $1
--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
-﻿script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 cd $script_dir
 source mypython/bin/activate
 mapproxy-util serve-multiapp-develop $1
