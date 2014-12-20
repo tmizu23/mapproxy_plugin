9cea277d77f8d5836d4d9ea89241802d176e1127
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2014-12-20 17:23:26 +0900

    update

--- bin/run.bat
+++ bin/run.bat
@@ -1,11 +1,11 @@
 cd /d %~dp0
 call mypython\Scripts\activate.bat
 
-REM ベクトルタイル用設定(mapnik)
-REM mapnikをインストールしたフォルダを指定してください
+::for vector tile(mapnik)
+::set mapnik folder
+
 set MAPNIK_HOME=C:\mapnik-v2.2.0
 
 set PATH=%MAPNIK_HOME%\lib;%MAPNIK_HOME%\bin;%PATH%
 set PYTHONPATH=%MAPNIK_HOME%\python\2.7\site-packages;%PYTHONPATH%
-
 start "mapproxy server"/min mapproxy-util serve-multiapp-develop %1
